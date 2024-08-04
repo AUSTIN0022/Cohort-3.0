de15420d8416eee3003660f068bfe96bcd68495d
Austin <95069137+AUSTIN0022@users.noreply.github.com>
2024-08-04 21:23:33 +0530

    Assignment on Object

---
+++ week 0/object.js
@@ -0,0 +1,40 @@
+/*  Assignment #1
+Write a function that takes a user as an input and greets them with their name and age
+*/
+function greet(user){
+    console.log(`Hello, ${user.name}! You are ${user.age} years old.`);
+}
+
+let user = {
+    name: "Austin",
+    age: 25
+}
+
+greet(user);
+
+/* Assignment #2
+Write a function that takes a new object as input which has name , age  and gender and greets the user with their gender (Hi Mr/Mrs/Others harkirat, your age is 21)
+*/
+function greet2(user){
+    let gender = "";
+
+    if(user.gender == "Male") {
+        gender = "Mr.";
+    } else if (user.gender == "Female") {
+        gender = "Mr.";
+    } else {
+        gender = "Others";
+    }
+
+    let canVote = user.age > 18 ? "can vote" : "can not vote";
+
+    console.log(`Hi ${gender} ${user.name}, You age is ${user.age}. and you ${canVote}`);
+}
+
+let user2 = {
+    name: "Austin",
+    age: 25,
+    gender: "Male",
+};
+
+greet2(user2);
